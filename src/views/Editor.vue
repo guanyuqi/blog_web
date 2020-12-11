35d17c26d992c4bbaab1e11a7e375264fbaa69c9
如斯 <37902939+guanyuqi@users.noreply.github.com>
2020-12-11 18:19:43 +0800

    富文本测试

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -3,10 +3,76 @@
     <router-link to="/">
       <h2>主页</h2>
     </router-link>
+    <div class="container">
+      <div class="editor">
+        <div id="editor-container"></div>
+        <button @click="getEditorData">获取内容</button>
+      </div>
+      <div class="preview" ref="preview"></div>
+    </div>
   </div>
 </template>
 <script>
-export default {}
+import wangEditor from 'wangeditor'
+export default {
+  data() {
+    return {
+      editor: null,
+      editorData: '请输入'
+    }
+  },
+  mounted() {
+    const editor = new wangEditor(`#editor-container`)
+
+    editor.config.height = 500
+    // 配置 onchange 回调函数，将数据同步到 vue 中
+    editor.config.onchange = newHtml => {
+      this.editorData = newHtml
+    }
+    // 创建编辑器
+    editor.create()
+    this.editor = editor
+  },
+  methods: {
+    getEditorData() {
+      console.log(this.editorData)
+      this.$refs.preview.innerHTML = this.editorData
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  .preview,
+  .editor {
+    flex: 1;
+    height: 100%;
+  }
+}
+.preview {
+  padding: 10px;
+  background-color: #f9f9f9;
+  p {
+    color: yellow;
+  }
+  pre {
+    position: relative;
+    padding-left: 10px;
+    counter-reset: linenumber;
+    word-wrap: normal;
+    word-break: break-all;
+    white-space: pre;
+    overflow-x: scroll;
+    overscroll-behavior-x: contain;
+    margin-top: 0;
+    margin-bottom: 20px;
+    border-radius: 4px;
+    z-index: 0;
+    padding: 1em;
+    line-height: 1.5;
+    color: #ccc;
+    background: #2d2d2d;
+  }
+}
 </style>
